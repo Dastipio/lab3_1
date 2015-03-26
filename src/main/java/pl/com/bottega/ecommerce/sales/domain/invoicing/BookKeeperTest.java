555777a2ced459d651822a0e7e47e38b839e619b
Daniel Stanczak <daniel_stanczak@hotmail.com>
2015-03-26 10:07:13 +0100

    Refaktoryzacja testu z zajęć 3. Utworzenie testu dla pierwszego przypadku.

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -4,8 +4,8 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import java.util.Date;
 import org.junit.Test;
-import pl.com.bottega.cqrs.command.handler.*;
-import pl.com.bottega.ecommerce.canonicalmodel.*;
+import static org.hamcrest.Matchers.*;
+
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
@@ -14,34 +14,43 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeperTest {
 
-	private BookKeeper book;
-	private Invoice invoice;
-
-	public void start() {
-		// given
-		InvoiceFactory factoryMock = mock(InvoiceFactory.class);
-		BookKeeper book = new BookKeeper(factoryMock);
-	}
-
 	@Test
 	public void testCase_1_DemandFactureWithOnePostition_ShouldReturnFactureWithOnePosition() {
-		
-		
+
 		Money money = new Money(20);
 		Id id = new Id("1");
-		InvoiceRequest invoiceRequest = new InvoiceRequest(new ClientData(new Id("1"),"facture"));
+		ClientData clientData = new ClientData(id, "klient1");
+		
+		BookKeeper book;
+		
 		TaxPolicy taxPolicy = mock(TaxPolicy.class);
-		ProductData productData = new ProductData(new Id("1"),new Money(10),"book",ProductType.DRUG,new Date(0));
-	
-		RequestItem requestItem = new RequestItem(productData, 20, new Money(20));
-		
-		book.issuance(invoiceRequest, taxPolicy);
-		//when(requestItem.getQuantity());
-		when(taxPolicy.calcuateTax(ProductType.DRUG, money)).thenReturn(new Tax(money,"spis"));
-		invoiceRequest.add(RequestItem);
 
-		@Test
-		public void testCase_2_DemandFactureWithOnePostition_ShouldReturnFactureWithOnePosition()
+		InvoiceFactory InvoiceFactoryMock = mock(InvoiceFactory.class);
+		when(InvoiceFactoryMock.create(clientData)).thenReturn(
+				new Invoice(id, clientData));
+		book = new BookKeeper(InvoiceFactoryMock);
+
+		InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+
+		when(taxPolicy.calculateTax(ProductType.FOOD, money)).thenReturn(
+				new Tax(money, "opis"));
+
+		ProductData productData = new ProductData(id, money, "book",
+				ProductType.FOOD, new Date(0));
+
+		RequestItem requestItem = new RequestItem(productData, 20, money);
+		invoiceRequest.add(requestItem);
 		
+		Invoice invoiceResult = book.issuance(invoiceRequest, taxPolicy);
+		int result = invoiceResult.getItems().size();
+
+		assertThat(result, is(1));
+
+		// @Test
+		// public void
+		// testCase_2_DemandFactureWithOnePostition_ShouldReturnFactureWithOnePosition(){
+		//
+		// }
+		//
 	}
 }
